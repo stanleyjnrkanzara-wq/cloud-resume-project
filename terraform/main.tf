e202781ada2b742bc5b070371422f19f756802e2
stanleyjnrkqnzara-wq <stanleyjnrkanzara@gmail.com>
2026-04-23 22:44:45 +0200

    add cloudfront distribution

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,3 +36,39 @@ resource "aws_s3_bucket_policy" "public_policy" {
     }]
   })
 }
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    domain_name = aws_s3_bucket.site.bucket_regional_domain_name
+    origin_id   = "s3-origin"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    target_origin_id       = "s3-origin"
+    viewer_protocol_policy = "allow-all"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
